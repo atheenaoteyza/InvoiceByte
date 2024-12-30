70804762445de4de781bfbb3177de79254e1d3e0
atheenaoteyza <alyboteyza@iskolarngbayan.pup.edu.ph>
2024-12-31 01:05:15 +0400

    feat: implement user login and invoice fetching on app load

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import AppRoutes from "./components/AppRoutes";
 import { BrowserRouter as Router } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setInvoiceByUserAsync } from "./redux/invoiceThunks";
+import { login } from "./redux/authSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const userId = sessionStorage.getItem("uid");
+
+  useEffect(() => {
+    if (userId) {
+      dispatch(login());
+      dispatch(setInvoiceByUserAsync(userId));
+    }
+  }, [dispatch, userId]);
+
   return (
     <Router>
       <AppRoutes></AppRoutes>
