2e3d69bb556a729260bcd52e881c1982d4f6a287
atheenaoteyza <alyboteyza@iskolarngbayan.pup.edu.ph>
2025-01-01 14:50:59 +0400

    fix: adjust invoice status layout

--- src/components/Elements/InvoiceStatus.jsx
+++ src/components/Elements/InvoiceStatus.jsx
@@ -2,22 +2,23 @@ import React from "react";
 
 export function InvoiceStatus({ type }) {
   const classNames = {
-    paid: ["text-[#33d69f] bg-[#33d69f0f]", "bg-[#33d69f]"],
-    pending: ["text-[#ff8f00] bg-[#ff8f000f]", "bg-[#ff8f00]"],
-    draft: ["text-[#dfe3fa] bg-[#dfe3fa0f]", "bg-[#dfe3fa]"],
+    paid: ["text-[#33d69f] bg-[#33d69f0f]", "bg-[#33d69f]", "px-7"], // Add px-7 for paid
+    pending: ["text-[#ff8f00] bg-[#ff8f000f]", "bg-[#ff8f00]", "px-4"], // Add px-4 for pending
+    draft: ["text-[#dfe3fa] bg-[#dfe3fa0f]", "bg-[#dfe3fa]", "px-4"], // Add px-4 for draft
   };
+
   return (
     <div
       className={`${
         type === "paid"
-          ? classNames.paid[0]
+          ? classNames.paid[0] + " " + classNames.paid[2] // Apply px-8 for paid
           : type === "pending"
-          ? classNames.pending[0]
-          : classNames.draft[0]
-      } flex justify-center space-x-2 rounded-lg  items-center px-4 py-2`}
+          ? classNames.pending[0] + " " + classNames.pending[2] // Apply px-4 for pending
+          : classNames.draft[0] + " " + classNames.draft[2] // Apply px-4 for draft
+      } flex justify-center space-x-2 rounded-lg items-center py-2`}
     >
       <div
-        className={` h-3 w-3 rounded-full  ${
+        className={`h-3 w-3 rounded-full  ${
           type === "paid"
             ? classNames.paid[1]
             : type === "pending"
