ee8ff0b421eb7afbe7e7e8694b0aaef89dcc7081
atheenaoteyza <alyboteyza@iskolarngbayan.pup.edu.ph>
2025-01-02 09:22:37 +0400

    fix: Darkmode transition icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class", // Enable dark mode
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          bg: "#ffffff",
+          text: "#000000",
+        },
+        dark: {
+          bg: "#1e2139",
+          text: "#ffffff",
+        },
+      },
+    },
   },
   plugins: [],
 };
